ef2aee33234b5a17fe231e11766e8a4d6f8ca7af
dijkstractiv-admin <alesio.puf@stud.ubbcluj.ro>
2024-11-16 20:08:10 +0200

    Added user handling

--- app/room/[roomId]/page.jsx
+++ app/room/[roomId]/page.jsx
@@ -9,21 +9,50 @@ const BACKEND_URL = "http://localhost:8080";
 const getRoomById = async (roomId) => {
   const response = await fetch(`${BACKEND_URL}/room/${roomId}`);
   const data = await response.json();
+  console.log("Fetched room data:", data);
   return data;
+};
+
+const toggleAttacker = async (roomId) => {
+  const response = await fetch(`${BACKEND_URL}/room/${roomId}/attacker`, {
+    method: "PATCH"
+  });
+};
+
+const toggleDefender = async (roomId) => {
+  const response = await fetch(`${BACKEND_URL}/room/${roomId}/defender`, {
+    method: "PATCH"
+  });
 };
 
 function Room() {
   const { roomId } = useParams();
   const [roomData, setRoomData] = useState();
+  const [attackerPresent, setAttackerPresent] = useState();
+  const [defenderPresent, setDefenderPresent] = useState();
 
   console.log("The room id is", roomId);
 
   useEffect(() => {
     if (roomId) {
       console.log("Fetching data for room ID:", roomId);
+
       getRoomById(roomId)
-        .then((data) => setRoomData(data))
-        .catch((error) => console.error("Failed to fetch room data:", error));
+      .then((data) => {
+        console.log("Fetched room data:", data);
+        setRoomData(data);
+
+        if (data.attackerPresent === false) {
+          console.log("Attacker is not present");
+          toggleAttacker(roomId);
+          setAttackerPresent(true);
+        } else if (data.defenderPresent === false) {
+          console.log("Defender is not present");
+          toggleDefender(roomId);
+          setDefenderPresent(true);
+        }
+      })
+      .catch((error) => console.error("Failed to fetch room data:", error));
     }
   }, [roomId]);
 
